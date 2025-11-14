b35afeb257c91d3520edf281c7c676851bdc98ad
Gene Marks <genemarks74@gmail.com>
2025-11-14 14:39:46 -0500

    initial NotifyIcon commit

--- src/FELauncher.Host/Program.cs
+++ src/FELauncher.Host/Program.cs
@@ -1,7 +1,21 @@
-﻿class Program
+﻿using FELauncher.Host.Tray;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+class Program
 {
+    [STAThread]
     static void Main(string[] args)
     {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder();
 
+        builder.Services.AddSingleton<NotifyIconManager>();
+
+        using IHost host = builder.Build();
+
+        host.Services.GetRequiredService<NotifyIconManager>();
+
+        host.Run();
     }
 }
---
+++ src/FELauncher.Host/Tray/NotifyIconManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace FELauncher.Host.Tray
+{
+    public class NotifyIconManager
+    {
+        private NotifyIcon notifyIcon;
+
+        public NotifyIconManager()
+        {
+            this.notifyIcon = new NotifyIcon
+            {
+                Icon = LoadEmbeddedIcon("FELauncher.Host.Assets.win_ico_16.ico"),
+                Text = "Test",
+                Visible = true
+            };
+
+        }
+
+        private Icon LoadEmbeddedIcon(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            return new Icon(stream);
+        }
+
+    }
+}
