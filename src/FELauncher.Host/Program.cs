5d93df68df1600300bc21f833f04ea33e911d48d
Gene Marks <genemarks74@gmail.com>
2025-11-19 16:00:00 -0500

    Boostrap settings file, initialize configuration with settings file, register configuration bind with FrontendSettings, register PathResolver service

--- src/FELauncher.Host/Program.cs
+++ src/FELauncher.Host/Program.cs
@@ -1,5 +1,9 @@
-﻿using FELauncher.Engine.Processes;
+﻿using FELauncher.Engine.IO;
+using FELauncher.Engine.Processes;
+using FELauncher.Engine.Settings;
+using FELauncher.Host.Bootstrap;
 using FELauncher.Host.Tray;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,8 +13,19 @@ class Program
     [STAThread]
     static void Main(string[] args)
     {
+        const string settingsFile = "felauncher.json";
+        FELauncherSettingsBootstrapper.EnsureSettingsFileExists(settingsFile);
+
         HostApplicationBuilder builder = Host.CreateApplicationBuilder();
 
+        builder.Configuration.Sources.Clear();
+        builder.Configuration
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile(settingsFile, optional: false, reloadOnChange: true);
+
+        builder.Services.Configure<FrontendSettings>(builder.Configuration.GetSection(key: "Frontend"));
+        
+        builder.Services.AddSingleton<IPathResolver, PathResolver>();
         builder.Services.AddSingleton<IProcessManager, ProcessManager>();
         builder.Services.AddSingleton<NotifyIconManager>();
 
