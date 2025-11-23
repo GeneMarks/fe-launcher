4754a99177e288a280497527779e552c248bdc38
Gene Marks <genemarks74@gmail.com>
2025-11-22 23:45:52 -0500

    Implement Exit() using IHostApplicationLifetime

--- src/FELauncher.Host/Tray/TrayController.cs
+++ src/FELauncher.Host/Tray/TrayController.cs
@@ -1,16 +1,22 @@
 ﻿using FELauncher.Engine.Processes;
 using FELauncher.Engine.Settings;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
 namespace FELauncher.Host.Tray
 {
     public class TrayController : ITrayController
     {
+        private readonly IHostApplicationLifetime _lifetime;
         private readonly IProcessManager _processManager;
         private readonly FrontendSettings _frontendSettings;
 
-        public TrayController(IProcessManager processManager, IOptionsMonitor<FrontendSettings> frontendSettings)
+        public TrayController(
+            IHostApplicationLifetime lifetime,
+            IProcessManager processManager,
+            IOptionsMonitor<FrontendSettings> frontendSettings)
         {
+            _lifetime = lifetime;
             _processManager = processManager;
             _frontendSettings = frontendSettings.CurrentValue;
         }
@@ -27,7 +33,7 @@ namespace FELauncher.Host.Tray
 
         public void Exit()
         {
-            return;
+            _lifetime.StopApplication();
         }
     }
 }
