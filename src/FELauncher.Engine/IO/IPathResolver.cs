11e827bfb7b4290376fcc9325a740cc775af58f0
Gene Marks <genemarks74@gmail.com>
2025-11-30 22:34:08 -0500

    Implement logging for IO scope. Add summary to IPathResolver.ResolvePath()

--- src/FELauncher.Engine/IO/IPathResolver.cs
+++ src/FELauncher.Engine/IO/IPathResolver.cs
@@ -2,6 +2,10 @@ namespace FELauncher.Engine.IO
 {
 	public interface IPathResolver
 	{
+		/// <summary>
+		/// If path is relative, add application directory to base.
+		/// Return resolved absolute path.
+		/// </summary>
 		string ResolvePath(string? path);
 		bool IsAbsolute(string path);
 	}
--- src/FELauncher.Engine/IO/PathResolver.cs
+++ src/FELauncher.Engine/IO/PathResolver.cs
@@ -1,23 +1,37 @@
+using FELauncher.Engine.Logging;
+using Microsoft.Extensions.Logging;
+
 namespace FELauncher.Engine.IO
 {
-	public class PathResolver : IPathResolver
+	public sealed class PathResolver : IPathResolver
 	{
+		private readonly ILogger<PathResolver> _logger;
 		private readonly string _basePath;
 
-		public PathResolver()
+		public PathResolver(ILogger<PathResolver> logger)
 		{
+			_logger = logger;
 			_basePath = AppContext.BaseDirectory;
 		}
 
 		public string ResolvePath(string? path)
 		{
 			if (string.IsNullOrEmpty(path))
-				return string.Empty;
+			{
+				_logger.IO_ResolverPathNullOrEmpty();
+				return String.Empty;
+			}
 
 			if (IsAbsolute(path))
+			{
+				_logger.IO_ResolverPathAbsolute(path);
 				return path;
+			}
 
-			return Path.GetFullPath(Path.Combine(_basePath, path));
+			var resolvedPath = Path.GetFullPath(Path.Combine(_basePath, path));
+
+			_logger.IO_ResolverResult(resolvedPath);
+			return resolvedPath;
 		}
 
 		public bool IsAbsolute(string path)
