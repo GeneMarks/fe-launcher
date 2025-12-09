363fdb34f3ab3054568f746605b181bd13a85e31
Gene Marks <genemarks74@gmail.com>
2025-12-09 14:13:42 -0500

    Add global project constants class for Host

---
+++ src/FELauncher.Host/HostConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace FELauncher.Host
+{
+    internal static class HostConstants
+    {
+        public const string MutexName    = @"Global\925bac46-79de-4db8-8b09-0ba589ce99ee";
+        public const string SettingsFile = "felauncher.json";
+    }
+}
