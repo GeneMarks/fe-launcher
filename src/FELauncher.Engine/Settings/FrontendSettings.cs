ef601575bc3121cd10ab5c024dd975ddf4b0a549
Gene Marks <genemarks74@gmail.com>
2025-12-09 14:15:28 -0500

    Update Settings to support preprocesses

--- src/FELauncher.Engine/Settings/FrontendSettings.cs
+++ src/FELauncher.Engine/Settings/FrontendSettings.cs
@@ -2,7 +2,8 @@ namespace FELauncher.Engine.Settings
 {
     public sealed class FrontendSettings
     {
-        public string FrontendPath { get; set; } = "";
-        public string? FrontendArguments { get; set; } = null;
+        public string Path { get; set; } = String.Empty;
+        public string? Arguments { get; set; }
+        public int DelaySeconds { get; set; }
     }
 }
---
+++ src/FELauncher.Engine/Settings/PreProcessSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace FELauncher.Engine.Settings
+{
+    public sealed class PreProcessSettings
+    {
+        public string Path { get; set; } = String.Empty;
+        public string? Arguments { get; set; }
+        public int DelaySeconds { get; set; }
+
+        public bool NotifyOnExit { get; set; }
+        public bool EndSessionOnExit { get; set; }
+    }
+}
