e252b6eea2d9526e7ac9d1b21b914232461b6765
Gene Marks <genemarks74@gmail.com>
2025-12-16 15:45:11 -0500

    Remove nullability from List<ProProcessSettings> PreProcesses

--- src/FELauncher.Engine/Settings/FELauncherSettings.cs
+++ src/FELauncher.Engine/Settings/FELauncherSettings.cs
@@ -3,6 +3,6 @@ namespace FELauncher.Engine.Settings
     public sealed class FELauncherSettings
     {
         public FrontendSettings Frontend { get; set; } = new();
-        public List<PreProcessSettings>? PreProcesses { get; set; } = new();
+        public List<PreProcessSettings> PreProcesses { get; set; } = [];
     }
 }
