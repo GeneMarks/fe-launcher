00f5cbf7b9d61c02b42e6cf4945b04deb5708645
Gene Marks <genemarks74@gmail.com>
2025-12-16 14:24:53 -0500

    Change access to internal, implement Status, Runtime, and IsActive properties

--- src/FELauncher.Engine/Sessions/Session.cs
+++ src/FELauncher.Engine/Sessions/Session.cs
@@ -1,4 +1,6 @@
-﻿namespace FELauncher.Engine.Sessions
+﻿using System.Diagnostics;
+
+namespace FELauncher.Engine.Sessions
 {
     public enum SessionStatus
     {
@@ -13,10 +15,53 @@
         Failed
     }
 
-    public sealed class Session
+    internal sealed class Session
     {
         public Guid Id { get; } = Guid.NewGuid();
         public DateTimeOffset CreatedAt { get; } = DateTimeOffset.UtcNow;
-        public SessionStatus Status { get; internal set; } = SessionStatus.Created;
+        public SessionStatus Status
+        {
+            get => _status;
+            internal set
+            {
+                _status = value;
+                if (_status is SessionStatus.Starting)
+                {
+                    _startTime = Stopwatch.GetTimestamp();
+                }
+                else if (!IsActive && _status is not SessionStatus.Created)
+                {
+                    _endTime = Stopwatch.GetTimestamp();
+                }
+            }
+        }
+
+        public TimeSpan Runtime
+        {
+            get
+            {
+                if (_status is SessionStatus.Created || _startTime == 0)
+                {
+                    return TimeSpan.Zero;
+                }
+
+                if (IsActive || _endTime == 0)
+                {
+                    return Stopwatch.GetElapsedTime(_startTime);
+                }
+
+                return Stopwatch.GetElapsedTime(_startTime, _endTime);
+            }
+        }
+
+        private SessionStatus _status = SessionStatus.Created;
+        private long _startTime;
+        private long _endTime;
+
+        public bool IsActive =>
+            Status is not (SessionStatus.Created
+                        or SessionStatus.Completed
+                        or SessionStatus.Aborted
+                        or SessionStatus.Failed);
     }
 }
