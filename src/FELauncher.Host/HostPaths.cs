6b6cb1b1461838b5a69385c46edfe3ba1a1ce9a6
Gene Marks <genemarks74@gmail.com>
2025-12-20 09:41:09 -0500

    Add HostPaths class

---
+++ src/FELauncher.Host/HostPaths.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace FELauncher.Host
+{
+    internal static class HostPaths
+    {
+        public const string AppDataDirectory       = "felauncher";
+        public static readonly string SettingsFile = Path.Combine(AppDataDirectory, "felauncher.json");
+        public static readonly string LogFile      = Path.Combine(AppDataDirectory, "felauncher.log");
+    }
+}
