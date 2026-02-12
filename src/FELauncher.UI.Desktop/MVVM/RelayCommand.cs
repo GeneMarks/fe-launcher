ceda0657b5b38b21d2b9bae058300c34dcbcacad
Gene Marks <genemarks74@gmail.com>
2026-02-12 16:15:50 -0500

    Implement sync and async RelayCommand for MVVM

---
+++ src/FELauncher.UI.Desktop/MVVM/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace FELauncher.UI.Desktop.MVVM
+{
+    internal sealed class RelayCommand : ICommand
+    {
+        private readonly Predicate<object?> _canExecute;
+        private readonly Action<object?> _execute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Predicate<object?> canExecute, Action<object?> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
