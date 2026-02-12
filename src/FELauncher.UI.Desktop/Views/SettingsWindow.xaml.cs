44c0c2b78393ae9c1d7b67f2def9b97efe99bcb4
Gene Marks <genemarks74@gmail.com>
2026-02-12 16:18:25 -0500

    Move SettingsWindow source files to Views/, implement view and codebehind

---
+++ src/FELauncher.UI.Desktop/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace FELauncher.UI.Desktop.Views
+{
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+    }
+}
