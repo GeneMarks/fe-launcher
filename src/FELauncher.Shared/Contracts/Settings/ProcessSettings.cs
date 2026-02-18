e804728c4cec9ff96602c7175aaa4847a8891c28
Gene Marks <genemarks74@gmail.com>
2026-02-18 12:04:46 -0500

    Add copy constructor to ProcessSettings

--- src/FELauncher.Shared/Contracts/Settings/ProcessSettings.cs
+++ src/FELauncher.Shared/Contracts/Settings/ProcessSettings.cs
@@ -7,5 +7,16 @@ namespace FELauncher.Shared.Contracts.Settings
         public int DelaySeconds { get; set; }
         public bool NotifyOnExit { get; set; }
         public bool EndSessionOnExit { get; set; }
+
+        public ProcessSettings() { }
+
+        public ProcessSettings(ProcessSettings ps)
+        {
+            Path = ps.Path;
+            Arguments = ps.Arguments;
+            DelaySeconds = ps.DelaySeconds;
+            NotifyOnExit = ps.NotifyOnExit;
+            EndSessionOnExit = ps.EndSessionOnExit;
+        }
     }
 }
