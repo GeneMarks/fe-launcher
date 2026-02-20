58693c2c178c072681f70d214f3d763ee42ad6c2
Gene Marks <genemarks74@gmail.com>
2026-02-20 15:45:50 -0500

    Add PreProcessWindow view and viewmodel

---
+++ src/FELauncher.UI.Desktop/ViewModels/PreProcessWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FELauncher.Shared.Contracts.Settings;
+
+namespace FELauncher.UI.Desktop.ViewModels
+{
+    internal sealed partial class PreProcessWindowViewModel : ObservableObject
+    {
+        public required ProcessSettings ProcessSettings { get; init; }
+    }
+}
